9aba3c55815fe7e18761bf09a175531d9c1ad0ba
L0L022 <L0L022@openmailbox.org>
2018-02-05 07:29:46 +0100

    moteur 3D tp 2 presque fini

--- moteurs_3D/tp2/main.cpp
+++ moteurs_3D/tp2/main.cpp
@@ -4,7 +4,13 @@
 #include <osg/PositionAttitudeTransform>
 #include <osgGA/NodeTrackerManipulator>
 #include <osgGA/GUIEventHandler>
+#include <osgDB/ReadFile>
+#include <osg/Texture2D>
+#include <osgViewer/ViewerEventHandlers>
+#include <osg/Switch>
+#include <osg/Camera>
 #include <iostream>
+#include <random>
 
 class SearchNode : public osg::NodeVisitor
 {
@@ -28,46 +34,20 @@ protected:
 class EventHandler : public osgGA::GUIEventHandler
 {
 public:
-    EventHandler(osg::ref_ptr<osg::StateSet> state, osg::ref_ptr<osg::Node> scene) : _state(state), _scene(scene), move_name(" ") {}
+    EventHandler(osg::ref_ptr<osg::StateSet> state, osg::ref_ptr<osg::Node> scene) : _state(state), _scene(scene) {}
 
     virtual bool handle(const osgGA::GUIEventAdapter &ea, osgGA::GUIActionAdapter &aa) {
-        auto direction = osg::Vec3d{0, 0, 0};
-
         switch (ea.getEventType()) {
         case osgGA::GUIEventAdapter::KEYDOWN:
             switch (ea.getKey()) {
-            case '1':
-                switchLight(GL_LIGHT1);
-                break;
-            case '2':
-                switchLight(GL_LIGHT2);
-                break;
-            case 'b':
-                move_name = "patBox";
-                break;
-            case 's':
-                move_name = "patSphere";
-                break;
-            case 'c':
-                move_name = "patCone";
-                break;
-            case osgGA::GUIEventAdapter::KEY_Up:
-                direction[1] += 1;
-                break;
-            case osgGA::GUIEventAdapter::KEY_Down:
-                direction[1] -= 1;
-                break;
-            case osgGA::GUIEventAdapter::KEY_Right:
-                direction[0] += 1;
-                break;
-            case osgGA::GUIEventAdapter::KEY_Left:
-                direction[0] -= 1;
-                break;
-            case osgGA::GUIEventAdapter::KEY_Page_Up:
-                direction[2] += 1;
-                break;
-            case osgGA::GUIEventAdapter::KEY_Page_Down:
-                direction[2] -= 1;
+            case 'v': {
+                SearchNode search("switch");
+                _scene->accept(search);
+                auto n = search.node();
+                if (n != nullptr) {
+                    switchSwitch(n->asSwitch());
+                }
+            }
                 break;
             default:
                 break;
@@ -78,136 +58,121 @@ public:
             break;
         }
 
-        SearchNode search(move_name);
-        _scene->accept(search);
-        auto n = search.node();
-        if (n != nullptr) {
-            auto pos = n->asTransform()->asPositionAttitudeTransform()->getPosition();
-            n->asTransform()->asPositionAttitudeTransform()->setPosition(pos + direction);
-        }
-
         return false;
     }
 
 private:
-    void switchLight(int light) {
-        _state->setMode(light, _state->getMode(light) == osg::StateAttribute::ON ? osg::StateAttribute::OFF : osg::StateAttribute::ON);
+    void switchSwitch(osg::Switch * sw) {
+        auto child = sw->getChild(0);
+        sw->setChildValue(child, !sw->getChildValue(child));
     }
 
 private:
     osg::ref_ptr<osg::StateSet> _state;
     osg::ref_ptr<osg::Node> _scene;
-    std::string move_name;
 };
 
 osg::Group* creation_troupeau(int nb_vaches, float taillex, float tailley)
 {
-    osg::ref_ptr<osg::Group> scene = new osg::Group;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> disX(0.f, taillex);
+    std::uniform_real_distribution<float> disY(0.f, tailley);
+    std::uniform_real_distribution<float> disDeg(0.f, 360.f);
+
+    osg::ref_ptr<osg::Node> vache_low = osgDB::readNodeFile("cow_low.3ds");
+    osg::ref_ptr<osg::Node> vache_mid = osgDB::readNodeFile("cow_mid.3ds");
+    osg::ref_ptr<osg::Node> vache_high = osgDB::readNodeFile("cow_high.3ds");
+
+    osg::ref_ptr<osg::LOD> lod = new osg::LOD;
+    lod->setRangeMode(osg::LOD::DISTANCE_FROM_EYE_POINT);
+    lod->addChild(vache_high, 0, 10);
+    lod->addChild(vache_mid, 10, 20);
+    lod->addChild(vache_low, 20, 1000);
+
+    osg::ref_ptr<osg::Group> vaches = new osg::Group;
     for (size_t i = 0; i < nb_vaches; ++i) {
         osg::ref_ptr<osg::PositionAttitudeTransform> pat = new osg::PositionAttitudeTransform;
-        pat->setPosition({i/taillex, i/tailley, 0});
-        scene->addChild(pat);
+        pat->setPosition({disX(gen), disY(gen), 0});
+        pat->setAttitude(osg::Quat(osg::DegreesToRadians(disDeg(gen)), osg::Vec3{0.f, 0.f, 1.f}));
+        pat->addChild(lod);
+        vaches->addChild(pat);
     }
-    return scene;
+    return vaches.release();
+}
+
+osg::Node* creation_sol(float taillex, float tailley)
+{
+    osg::ref_ptr<osg::Texture2D> texture = new osg::Texture2D;
+    texture->setImage(osgDB::readImageFile("herbe.jpg"));
+    texture->setFilter(osg::Texture::MIN_FILTER, osg::Texture::LINEAR);
+    texture->setFilter(osg::Texture::MAG_FILTER, osg::Texture::LINEAR);
+    texture->setWrap(osg::Texture::WRAP_S, osg::Texture::REPEAT);
+    texture->setWrap(osg::Texture::WRAP_T, osg::Texture::REPEAT);
+
+    osg::ref_ptr<osg::Geometry> quad = osg::createTexturedQuadGeometry({0.f, 0.f, 0.f}, {taillex, 0.f, 0.f}, {0.f, tailley, 0.f}, 0.f, 0.f, 1.f, 1.f);
+    quad->getOrCreateStateSet()->setTextureAttributeAndModes(0, texture);
+    quad->getOrCreateStateSet()->setMode(GL_DEPTH_TEST, osg::StateAttribute::ON);
+
+    osg::ref_ptr<osg::Geode> geode = new osg::Geode;
+    geode->addDrawable(quad);
+
+    return geode.release();
 }
 
 int main(int argc, char *argv[])
 {
     osg::DisplaySettings::instance()->setNumMultiSamples(8);
 
-    osg::ref_ptr<osg::Shape> shapeBox = new osg::Box({0, 0, 0}, 2, 3, 4);
-    osg::ref_ptr<osg::Material> materialBox = new osg::Material;
-    materialBox->setAmbient(osg::Material::FRONT_AND_BACK, {0.5, 0, 0, 1});
-    materialBox->setDiffuse(osg::Material::FRONT_AND_BACK, {0.9, 0, 0, 1});
-    materialBox->setSpecular(osg::Material::FRONT_AND_BACK, {0.2, 0.2, 0.2, 1});
-    materialBox->setShininess(osg::Material::FRONT_AND_BACK, 64);
-    osg::ref_ptr<osg::ShapeDrawable> shapeDrawableBox = new osg::ShapeDrawable(shapeBox);
-    shapeDrawableBox->getOrCreateStateSet()->setAttributeAndModes(materialBox);
-    osg::ref_ptr<osg::Geode> geodeBox = new osg::Geode;
-    geodeBox->addDrawable(shapeDrawableBox);
+    osg::ref_ptr<osg::Group> vaches = creation_troupeau(1000, 100, 100);
 
-    osg::ref_ptr<osg::PositionAttitudeTransform> patBox = new osg::PositionAttitudeTransform;
-    patBox->setName("patBox");
-    patBox->setPosition({0, 0, 0});
-    patBox->addChild(geodeBox);
+    osg::ref_ptr<osg::Switch> switchNode = new osg::Switch;
+    switchNode->setName("switch");
+    switchNode->addChild(vaches);
 
-    osg::ref_ptr<osg::Shape> shapeSphere = new osg::Sphere({0, 0, 0}, 1);
-    osg::ref_ptr<osg::Material> materialSphere = new osg::Material;
-    materialSphere->setAmbient(osg::Material::FRONT_AND_BACK, {0.5, 0.5, 0, 1});
-    materialSphere->setDiffuse(osg::Material::FRONT_AND_BACK, {1, 1, 0, 1});
-    materialSphere->setSpecular(osg::Material::FRONT_AND_BACK, {0.2, 0.2, 0.2, 1});
-    materialSphere->setShininess(osg::Material::FRONT_AND_BACK, 64);
-    osg::ref_ptr<osg::ShapeDrawable> shapeDrawableSphere = new osg::ShapeDrawable(shapeSphere);
-    shapeDrawableSphere->getOrCreateStateSet()->setAttributeAndModes(materialSphere);
-    osg::ref_ptr<osg::Geode> geodeSphere = new osg::Geode;
-    geodeSphere->addDrawable(shapeDrawableSphere);
-
-    osg::ref_ptr<osg::PositionAttitudeTransform> patSphere = new osg::PositionAttitudeTransform;
-    patSphere->setName("patSphere");
-    patSphere->setPosition({5, 0, 0});
-    patSphere->addChild(geodeSphere);
-
-    osg::ref_ptr<osg::Shape> shapeCone = new osg::Cone({0, 0, 0}, 1, 2);
-    osg::ref_ptr<osg::Material> materialCone = new osg::Material;
-    materialCone->setAmbient(osg::Material::FRONT_AND_BACK, {0.5, 0, 0.5, 1});
-    materialCone->setDiffuse(osg::Material::FRONT_AND_BACK, {1, 0, 1, 1});
-    materialCone->setSpecular(osg::Material::FRONT_AND_BACK, {0.2, 0.2, 0.2, 1});
-    materialCone->setShininess(osg::Material::FRONT_AND_BACK, 64);
-    osg::ref_ptr<osg::ShapeDrawable> shapeDrawableCone = new osg::ShapeDrawable(shapeCone);
-    shapeDrawableCone->getOrCreateStateSet()->setAttributeAndModes(materialCone);
-    osg::ref_ptr<osg::Geode> geodeCone = new osg::Geode;
-    geodeCone->addDrawable(shapeDrawableCone);
-
-    osg::ref_ptr<osg::PositionAttitudeTransform> patCone = new osg::PositionAttitudeTransform;
-    patCone->setName("patCone");
-    patCone->setPosition({0, 5, 0});
-    patCone->addChild(geodeCone);
+    osg::ref_ptr<osg::Node> sol = creation_sol(100, 100);
 
     osg::ref_ptr<osg::LightSource> light1 = new osg::LightSource;
     light1->getLight()->setLightNum(1);
-    light1->getLight()->setPosition({1, 1, 1, 0});
+    light1->getLight()->setPosition({0, 0, 1, 0});
     light1->getLight()->setAmbient({0.5, 0.5, 0.5, 1});
     light1->getLight()->setDiffuse({1, 1, 1, 1});
     light1->getLight()->setSpecular({0.2, 0.2, 0.2, 1});
 
-    osg::ref_ptr<osg::LightSource> light2 = new osg::LightSource;
-    light2->getLight()->setLightNum(2);
-    light2->getLight()->setPosition({1, 1, 1, 1});
-    light2->getLight()->setAmbient({0, 0.5, 0.5, 1});
-    light2->getLight()->setDiffuse({0, 1, 1, 1});
-    light2->getLight()->setSpecular({0.2, 0.2, 0.2, 1});
-
     osg::ref_ptr<osg::Group> lights = new osg::Group;
     lights->addChild(light1);
-    lights->addChild(light2);
 
     osg::ref_ptr<osg::Group> scene = new osg::Group;
     scene->addChild(lights);
-    scene->addChild(patBox);
-    scene->addChild(patSphere);
-    scene->addChild(patCone);
+    scene->addChild(switchNode);
+    scene->addChild(sol);
 
     osg::ref_ptr<osg::StateSet> state = scene->getOrCreateStateSet();
-    state->setMode(GL_LIGHT0, osg::StateAttribute::OFF);
     state->setMode(GL_LIGHT1, osg::StateAttribute::ON);
-    state->setMode(GL_LIGHT2, osg::StateAttribute::ON);
 
     osg::ref_ptr<osgGA::NodeTrackerManipulator> manip = new osgGA::NodeTrackerManipulator;
-    manip->setTrackNode(geodeCone);
+    manip->setTrackNode(vaches);
     manip->setTrackerMode(osgGA::NodeTrackerManipulator::NODE_CENTER);
 
     osg::ref_ptr<EventHandler> eventHandler = new EventHandler(state, scene);
+
+    osg::ref_ptr<osg::Camera> cam1 = new osg::Camera;
+    cam1->setClearColor({0, 0, 0, 1});
+//    cam1->setProjectionMatrixAsPerspective(30, 4.f/3.f, 0.1, 100);
+//    cam1->setViewMatrixAsLookAt({0, -10, 10}, {0, 0, 0}, {0, 0, 1});
 
     osgViewer::Viewer viewer;
     viewer.setUpViewInWindow(100, 50, 800, 600);
     viewer.getCamera()->setClearColor({1, 1, 1, 1});
     viewer.setSceneData(scene);
-    viewer.setCameraManipulator(manip);
     viewer.addEventHandler(eventHandler);
+    viewer.addEventHandler(new osgViewer::StatsHandler);
+    viewer.setRunMaxFrameRate(30.0);
+//    viewer.setCamera(cam1);
 
     osgViewer::Viewer::Windows windows;
     viewer.getWindows(windows);
-    windows[0]->setWindowName("TP 1");
+    windows[0]->setWindowName("TP 2");
 
     return viewer.run();
 }
